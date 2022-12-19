e22ca0020d3d878fae7a1fc934510ba261090d66
Kyu Lee <kwooler@gmail.com>
2022-12-18 23:48:35 -0800

    A24

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    str = 'Python Programming'
-    sub1 = str[:6]
-    sub2 = str[-11:]
-    print(sub2)
-    print(sub1)
-    print(sub2+' '+sub1)
     pass
 
 
